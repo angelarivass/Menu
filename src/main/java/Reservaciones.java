81b216bd50a770b9518bbd0aabd8838de3e6d8fc
andreamoranamao <andrea.moran@cetys.edu.mx>
2024-05-20 16:59:18 -0700

    cambios

---
+++ src/main/java/Reservaciones.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author andreamoran
+ */
+
+import java.util.ArrayList;
+
+public class Reservaciones {
+    
+    private ArrayList<Reservacion> reservacion;
+    
+    public Reservaciones(){
+        
+        reservacion = new ArrayList<>();
+        
+    }
+    
+    public void crearReservacion(Reservacion reservacion){
+        
+        this.reservacion.add(reservacion);
+        
+    }
+    
+    public void cancelarReservacion(Reservacion reservacion){
+        
+        this.reservacion.remove(reservacion);
+        
+    }
+    
+}
